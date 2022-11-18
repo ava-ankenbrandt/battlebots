8197d2d51f158c692843ca8616c4df7849a510b5
ava-ankenbrandt <108041676+ava-ankenbrandt@users.noreply.github.com>
2022-11-18 00:18:25 -0800

    General layout sizes working

--- battlewebs/src/routes/+page.svelte
+++ battlewebs/src/routes/+page.svelte
@@ -5,21 +5,50 @@
     let windowHeight: number;
 
     onMount(async () => {
-		if ((windowWidth > 600) || (true)) {
-
+		if (windowWidth < windowHeight) {
+            alert('Please use landscape mode!');
         }
 	});
 </script>
 
 <svelte:window bind:innerWidth={windowWidth} bind:innerHeight={windowHeight}/>
 
-<h1 class="w-full text-center">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<div class="flex w-full">
-    <div class="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">content</div>
-    <div class="divider divider-horizontal"></div>
-    <div class="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">content</div>
-    <div class="divider divider-horizontal"></div>
-    <div class="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">content</div>
+<div class="flex w-screen h-screen">
+    <div class="grid h-full aspect-square bg-base-100 place-items-center max-w-[600px] border-slate-900 border-2">content</div>
+
+    
+    <div class="flex-1 flex justify-center items-center align-middle">
+        <!-- <div 
+        class="aspect-square w-[min(100vh,calc((50vw-7.5rem)))] h-[min(100vh,calc((50vw-7.5rem)))]"> -->
+        <div class="p-0 flex grow h-[100vh]">
+            <div class="grid grid-cols-2 gap-4 p-2 w-full h-full">
+                <div class="card bg-primary shadow-xl">
+                    <div class="card-body items-center text-center text-primary-content">
+                        A
+                    </div>
+                </div>
+                <div class="card bg-primary shadow-xl">
+                    <div class="card-body items-center text-center text-primary-content">
+                        A
+                    </div>
+                </div>
+                <div class="card bg-primary shadow-xl">
+                    <div class="card-body items-center text-center text-primary-content">
+                        A
+                    </div>
+                </div>
+                <div class="card bg-primary shadow-xl">
+                    <div class="card-body items-center text-center text-primary-content">
+                        A
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+    </div>
+    
+
+    <div class="flex-col h-20 w-20 card bg-base-300 rounded-box place-items-center">content3</div>
   </div>
 
